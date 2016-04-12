6576948e0bda5115fd83c774102f31355fb3544d
conner <c9er99@gmail.com>
2016-04-12 10:55:56 -0700

    it will add bower.json file

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,18 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Food Tracker</h1>
+      <h3>{{ food.name }}<h3>
+      <h5>{{ food.details }}<h5>
+      <h4>{{ food.calories }}<h4>
     </div>
   `
 })
 export class AppComponent {
 
+}
+export class Food {
+  public food: Food;
+  constructor(public name: string, public details: string, public calories: number ) {
+    this.food = new Food("Cheeseburger", "Double Cheese and triple bacon", 400)
+  }
 }
